63c4f61e6c12cb0db98f214a85a6f9e22c9590d2
hatguy33 <maderas@s.sfusd.edu>
2024-05-20 22:52:29 -0700

    portal (fixing 2)

---
+++ Portal.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+signal playerTP
+
+@export var player_position_x : int
+@export var player_position_y : int
+
+
+func _on_body_entered(body):
+	playerTP.emit()
+	print ("ok")
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,13 @@
 extends Node2D
+@onready var player = $Player
+@onready var portal = $Portal
 
-
+func _ready():
+	portal.playerTP.connect(go_TP)
 func _enter_tree():
 	if Checkpoint.last_position:
 		$Player.global_position = Checkpoint.last_position
+
+func go_TP():
+	player.position.x = portal.player_position_x
+	player.position.y = portal.player_position_y
